e7392096942dc36c9431b7719af45cb93a61fb7a
Tarun chintakunta <tarunchintakunta@gmail.com>
2025-04-12 19:15:39 +0530

    Add main application structure with routing and styling

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// In App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { Web3Provider } from './contexts/Web3Context';
+
+// Pages
+import LandingPage from './pages/LandingPage';
+import DashboardPage from './pages/DashboardPage';
+import SportEventsPage from './pages/SportEventsPage';
+import TicketBookingPage from './pages/TicketBookingPage';
+import PaymentPage from './pages/PaymentPage';
+import MyTicketsPage from './pages/MyTicketsPage';
+
+// Components
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Web3Provider>
+      <Router>
+        <Toaster position="top-center" />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/sports/:sport" element={<SportEventsPage />} />
+          <Route path="/book/:matchId" element={<TicketBookingPage />} />
+          <Route path="/payment/:matchId/:quantity" element={<PaymentPage />} />
+          <Route path="/my-tickets" element={<MyTicketsPage />} />
+          {/* Add a catch-all route to redirect to dashboard */}
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </Router>
+    </Web3Provider>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Web3Provider } from './contexts/Web3Context';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Web3Provider>
+      <App />
+    </Web3Provider>
+  </React.StrictMode>,
+);
